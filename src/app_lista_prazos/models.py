68fb8a5a8ff564133bd4342bbd74021c6b19fad9
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-03-28 20:04:31 -0300

    feat: model de Prazo adicionado, ListView genérica adicionada para exibição básica

--- src/app_lista_prazos/models.py
+++ src/app_lista_prazos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
+class Prazo(models.Model):
+    id = models.CharField(max_length=8, primary_key=True)
+    prazo = models.PositiveSmallIntegerField(default=0)
