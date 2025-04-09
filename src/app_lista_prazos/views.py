b405b5a53bbb52738aba01873f6877af10ddc28e
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-04-09 01:02:53 -0300

    wip: checkbox para calcular data de vencimento e model consertado

--- src/app_lista_prazos/views.py
+++ src/app_lista_prazos/views.py
@@ -1,7 +1,9 @@
+from datetime import timedelta
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from app_lista_prazos.forms import PrazoForm
 from app_lista_prazos.models import Prazo
+from django.utils import timezone
 
 
 # Create your views here.
@@ -11,9 +13,17 @@ def index(request):
 
 def form_adicionar(request):
     if request.method == "POST":
+        id = request.POST["id"]
+        prazo_em_dias = int(request.POST["prazo_em_dias"])
+
+        print(request.POST)
+
         add_prazo = Prazo(
-            id=request.POST["id"], prazo_em_dias=int(request.POST["prazo_em_dias"])
+            id,
+            prazo_em_dias,
+            data_de_vencimento=timezone.now() + timedelta(days=prazo_em_dias),
         )
+
         add_prazo.save()
 
     form = PrazoForm()
