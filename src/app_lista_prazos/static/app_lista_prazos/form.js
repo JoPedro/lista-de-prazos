cb4b55686914ab0189a3474e620ef4e556fa8103
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-04-27 17:09:01 -0300

    feat: estilização completa e fusão das views em uma só página

--- src/app_lista_prazos/static/app_lista_prazos/form.js
+++ src/app_lista_prazos/static/app_lista_prazos/form.js
@@ -1,11 +1,18 @@
-function changeDateInput() {
-    const checkbox = document.getElementsByName('agora')[0]
-    const dateInput = document.getElementsByName('data_de_vencimento')[0]
+const cadastrar = document.getElementById('cadastrar')
+const fechar = document.getElementById('fechar')
+const checkbox = document.getElementsByName('agora')[0]
+const dateInput = document.getElementsByName('data_de_vencimento')[0]
+const aside = document.getElementsByTagName('aside')[0]
+const overlay = document.getElementsByClassName('overlay')[0]
 
-    // Checar ao carregar página
-    dateInput.disabled = checkbox.checked
+cadastrar.addEventListener('click', toggleSidebarDisplay)
+fechar.addEventListener('click', toggleSidebarDisplay)
 
-    checkbox.addEventListener('change', function () {
-        dateInput.disabled = this.checked;
-    })
+checkbox.addEventListener('change', function () {
+    dateInput.disabled = this.checked
+})
+
+function toggleSidebarDisplay() {
+    aside.style.display = aside.style.display === 'block' ? 'none' : 'block'
+    overlay.style.display = overlay.style.display === 'block' ? 'none' : 'block'
 }
--- src/app_lista_prazos/urls.py
+++ src/app_lista_prazos/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PrazoListView.as_view(), name="prazo-list"),
-    path("adicionar", views.form_adicionar, name="adicionar"),
+    path("", views.index, name="prazo_list"),
 ]
