e2d298567162cced73e560b9caa64642395676e2
João Pedro <joaopedrosilvaqueiroz@gmail.com>
2025-04-28 21:29:04 -0300

    wip: delete view

--- src/app_lista_prazos/views.py
+++ src/app_lista_prazos/views.py
@@ -1,5 +1,6 @@
 from django.core.paginator import Paginator
 from datetime import datetime, timedelta
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from app_lista_prazos.forms import PrazoForm
 from app_lista_prazos.models import Prazo
@@ -7,27 +8,6 @@ from django.utils import timezone
 
 
 def index(request):
-    # Formulário enviado
-    if request.method == "POST":
-        id = request.POST.get("identificador")
-        prazo_em_dias = int(request.POST.get("prazo_em_dias"))
-
-        data_de_vencimento = request.POST.get(
-            "data_de_vencimento"
-        ) or timezone.now().replace(
-            tzinfo=timezone.get_current_timezone(), hour=0, minute=0, second=0
-        ) + timedelta(
-            days=prazo_em_dias
-        )
-
-        if type(data_de_vencimento) == str:
-            data_de_vencimento = datetime.strptime(
-                data_de_vencimento, "%d/%m/%Y"
-            ).replace(tzinfo=timezone.get_current_timezone())
-
-        add_prazo = Prazo(id, prazo_em_dias, data_de_vencimento)
-        add_prazo.save()
-
     # Lista paginada
     prazo_list = Prazo.objects.all().order_by("data_de_vencimento")
     paginator = Paginator(prazo_list, 10)
@@ -40,6 +20,39 @@ def index(request):
 
     return render(
         request,
-        "app_lista_prazos/prazo_list.html",
+        "app_lista_prazos/index.html",
         {"page_obj": page_obj, "form": form},
     )
+
+
+def adicionar(request):
+    id = request.POST.get("identificador")
+    prazo_em_dias = int(request.POST.get("prazo_em_dias"))
+
+    data_de_vencimento = request.POST.get(
+        "data_de_vencimento"
+    ) or timezone.now().replace(
+        tzinfo=timezone.get_current_timezone(), hour=0, minute=0, second=0
+    ) + timedelta(
+        days=prazo_em_dias
+    )
+
+    if type(data_de_vencimento) == str:
+        data_de_vencimento = datetime.strptime(data_de_vencimento, "%d/%m/%Y").replace(
+            tzinfo=timezone.get_current_timezone()
+        )
+
+    add_prazo = Prazo(id, prazo_em_dias, data_de_vencimento)
+    add_prazo.save()
+
+    return HttpResponseRedirect("/")
+
+
+def excluir(request, pk):
+    if request.method == "POST":
+        return HttpResponseRedirect("/")
+
+    prazo = Prazo.objects.get(identificador=pk)
+    context = {"prazo": prazo}
+
+    return render(request, "app_lista_prazos/excluir.html", context)
